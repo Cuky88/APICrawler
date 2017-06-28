13c7f55af08c99d670289d9b99a8635b949dc4da
Selcuk Aklan <Cuky88@users.noreply.github.com>
2017-06-28 22:04:01 +0200

    Fix dict creation to include exceptions

--- api_docs/api_docs/spiders/testURL.py
+++ api_docs/api_docs/spiders/testURL.py
@@ -3,7 +3,6 @@ import json
 import sys
 import urllib2
 import httplib
-
 import time
 from pprint import pprint
 
@@ -13,43 +12,47 @@ def main(argv):
 
     with open('dump_v1.json') as reader:
         read = json.load(reader)
-        for i,obj in enumerate(read):
+        start_time = time.time()
+        for i, obj in enumerate(read):
             dic = {}
+            if i < 5:
+                if 'api_url' in obj:
+                    host = obj["api_url"]
+                    url = obj["api_url_full"]
+                    title = obj["progweb_title"]
 
-            if 'api_url' in obj:
-                host = obj["api_url"]
-                url = obj["api_url_full"]
-                title = obj["progweb_title"]
+                    if title not in lines:
+                        dic['api_url'] = host
+                        dic['api_url_full'] = url
+                        dic['progweb_title'] = title
 
-                # print("OBJ: %s", obj)
+                        try:
+                            a = urllib2.urlopen(url)
 
-                if title not in lines:
-                    dic['api_url'] = host
-                    dic['api_url_full'] = url
-                    dic['progweb_title'] = title
+                            print(str(i) + " Status: " + str(a.getcode()))
 
-                    try:
-                        a = urllib2.urlopen(url)
+                            dic['error'] = 0
 
-                        if i % 10 == 0:
-                            print(str(i) + " .....Working....Status: " + a.getcode())
+                            lines.append(dic)
+                        except (urllib2.HTTPError, ValueError) as e:
+                            dic['error'] = 1
+                            lines.append(dic)
+                            continue
+                        except urllib2.URLError as e:
+                            dic['error'] = 2
+                            lines.append(dic)
+                            continue
+                        except httplib.BadStatusLine as e:
+                            dic['error'] = 3
+                            lines.append(dic)
+                            continue
+                        except:
+                            dic['error'] = 4
+                            lines.append(dic)
+                            continue
 
-                        dic['error'] = 0
-                    except (urllib2.HTTPError, ValueError) as e:
-                        dic['error'] = 1
-                        continue
-                    except urllib2.URLError as e:
-                        dic['error'] = 2
-                        continue
-                    except httplib.BadStatusLine as e:
-                        dic['error'] = 3
-                        continue
-                    except:
-                        dic['error'] = 4
-                        continue
-
-                    lines.append(dic)
-                    # print("LINES: %s", lines)
+        elapsed_time = time.time() - start_time
+        print('It took ' + str(elapsed_time) + ' seconds')
 
     with jsonlines.open('dump.json', mode='w') as writer:
         # json.dump(lines, writer, indent=2)
