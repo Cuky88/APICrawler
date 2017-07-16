02d3cc8885cc1128cf0db275cc3747319996d676
carolinedieterich <caroline@dieterich-home.de>
2017-07-16 17:40:20 +0200

    for Marce: new Reader and output format

---
+++ VecPCAClustering/src/DataSet.py
@@ -0,0 +1,6 @@
+class DataSet:
+
+    def __init__(self, a,d,n):
+        self.apiNr = a
+        self.name = n
+        self.descr = d
--- VecPCAClustering/src/Reader.py
+++ VecPCAClustering/src/Reader.py
@@ -1,17 +1,58 @@
 import json
 import re
+import numpy
+from DataSet import DataSet
 
 
-def load_descr():
-    with open('/Users/hanche/Google Drive/Studium/InWi Master/Seminar/KDD/APICrawler/VecPCAClustering/resources/final.json') as data_file:
-        data = json.load(data_file)
-    descr=[]
+def load_descr(dataset):
+    apiNr = []
+    descr = []
+    namen = []
 
-    for x in xrange(0, len(data)):
-        if data[str(x)].get("progweb_descr"):
-            text = data[str(x)]["progweb_descr"]
-            output = re.sub(r'\d+', '', text)
-            descr.append(output)
-    return descr
+    #load Programmable Web descriptions
+    if dataset == "prog":
+        with open('/home/caro/Documents/KDD/VecPCAClustering/resources/ProgWeb_preprocessed.json') as data_file:
+            data = json.load(data_file)
+
+        for x in xrange(0, len(data)):
+            if ("id" in data[x]) & ("progweb_descr" in data[x]) & ("api_name" in data[x]):
+                apiNr.append(data[x]["id"])
+                namen.append(data[x]["api_name"])
+
+                text = data[x]["progweb_descr"]
+                #remove all digets
+                output = re.sub(r'\d+', '', text)
+                descr.append(output)
+
+                dsJson = DataSet(apiNr, descr, namen)
+        return dsJson
+
+    else:
+        with open('/home/caro/Documents/APICrawler/VecPCAClustering/resources/complete.json') as data_file:
+            data = json.load(data_file)
+
+        for x in xrange(0, len(data)):
+            if "id" in data[x] & ("descr1" in data[x] | "descr2" in data[x] | "descr3" in data[x] | "descr4" in data[x] | "descr5" in data[x]):
+                apiNr.append(data[x]["id"])
+                text = ""
+                if "descr1" in data[x]:
+                    text = text + " " +data[x].get("descr1")
+                if "descr2" in data[x]:
+                    text = text + " " +data[x].get("descr2")
+                if "descr3" in data[x]:
+                    text = text + " " +data[x].get("descr3")
+                if "descr4" in data[x]:
+                    text = text + " " +data[x].get("descr4")
+                if "descr5" in data[x]:
+                    text = text + " " +data[x].get("descr5")
+
+                # remove all digets
+                output = re.sub(r'\d+', '', text)
+                descr.append(output)
+                dsJson = DataSet(apiNr, descr)
+
+        return dsJson
+
+
 
 
