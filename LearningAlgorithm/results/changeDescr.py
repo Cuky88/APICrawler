3fe5070cfa61be4b0aa26c28abb455023d556d65
Selcuk Aklan <Cuky88@users.noreply.github.com>
2017-07-20 15:27:07 +0200

    Data for website with descr

---
+++ LearningAlgorithm/results/changeDescr.py
@@ -0,0 +1,34 @@
+import json
+import sys
+import re
+
+def fullDescr(file1, file2):
+    descr = []
+    tmp = []
+
+    with open(file2) as reader:
+        read = json.load(reader)
+        for t in read:
+            tmp.append(t)
+
+    with open(file1) as reader:
+        read = json.load(reader)
+        for a in read:
+            for b in tmp:
+                if a['id'] == b['id']:
+                    descr.append({'api_name': a['api_name'], 'id': a['id'], 'cluster_id': a['cluster_id'], 'progweb_descr': b['progweb_descr']})
+
+    with open('cosine_500_500_descr.json', mode='w') as writer:
+        json.dump(descr, writer, indent=2)
+        writer.write('\n')
+
+
+def main(argv):
+    file1 = 'final_results/1_run/kmeansresults/cosine_500_500.json'
+    file2 = '../../api_docs/progweb_final_filtered.json'
+
+    fullDescr(file1, file2)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
